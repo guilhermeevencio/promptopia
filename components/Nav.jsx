35dd12ec68dca43fb75877e7edc76f0fac6316e1
guilhermeevencio <guilhermeevencioluz@gmail.com>
2023-05-16 13:11:39 -0300

    feat: finnishes user auth and create user database in mongoDB atlas.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,17 @@ import { useState, useEffect } from 'react'
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
-  const isUserLoggedIn = true
+  const { data: session } = useSession()
 
   const [providers, setProviders] = useState(null)
   const [toggleDropDown, setToggleDropdown] = useState(false)
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setupProviders = async () => {
       const response = await getProviders()
       setProviders(response)
     }
-    setProviders()
+    setupProviders()
   }, [])
 
   return (
@@ -34,7 +34,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-propt" className="black_btn">
               Create Post
@@ -42,7 +42,7 @@ const Nav = () => {
             <button type="button" onClick={signOut} className="outline_btn">Sign Out</button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
@@ -72,10 +72,10 @@ const Nav = () => {
       {/* MObile Navigation */}
       <div className="sm:hidden flex relative">
         {
-          isUserLoggedIn ? (
+          session?.user ? (
             <div className="flex">
               <Image
-                src="./assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -103,7 +103,7 @@ const Nav = () => {
                     className='mt-5 w-full black_btn'
                     onClick={() => {
                       setToggleDropdown(false)
-                      signOut
+                      signOut()
                     }}
                   >
                     Sign Out
